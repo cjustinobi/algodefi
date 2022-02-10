0c4ef0ebc96f107428ef9c5c920de120895aa309
cjustinobi <cjustinobi@gmail.com>
2022-02-10 06:43:39 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom'
+// import 'antd/dist/antd.css'
+import App from './App'
+import store from './redux/store'
+import { Provider } from 'react-redux'
+import GlobalStyle from './assets/css/globalStyle'
+import reportWebVitals from './reportWebVitals'
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <StrictMode>
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
+  </StrictMode>,
   document.getElementById('root')
 );
 
@@ -15,3 +23,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
